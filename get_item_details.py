a892828314eb2535a06e834ca921c9b17c05e2ab
Wheeler Weise <wheeler.weise@gmail.com>
2019-10-16 09:47:36 -0500

    Now able to find the search-results node.

--- get_item_details.py
+++ get_item_details.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import etree
+import xml.etree.ElementTree as ET
 
 base_url = 'https://www.rei.com/c/backpacking-tents?r=c%3Bbest-use%3ABackpacking&pagesize=90&ir=category%3Abackpacking-tents&sort=max-price'
 
@@ -13,8 +14,11 @@ if __name__ == '__main__':
         print("got website content successfully")
     
     search_page_body = search_page_resonse.content.decode(encoding='utf-8')
-    as_split = search_page_body.splitlines()
+    # as_split = search_page_body.splitlines()
     parser = etree.HTMLParser()
     tree = etree.fromstring(search_page_body, parser)
-    tree.
+    search_results = tree.xpath('//div[@id=\'search-results\']')
+    # orig_serialized = etree.tostring(tree)
+    # error_corrected_tree = ET.fromstring(orig_serialized)
+    # search_results = error_corrected_tree.find('div[@id="search-results"]')
     pass
