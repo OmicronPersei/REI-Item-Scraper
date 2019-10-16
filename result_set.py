23113fd9fcc7bb0c54cf84567f308ccf0c12029d
Wheeler Weise <wheeler.weise@gmail.com>
2019-10-16 14:26:07 -0500

    Added ability to detect sleep capacity and filter on it.

---
+++ result_set.py
@@ -0,0 +1,38 @@
+import json
+
+if __name__ == '__main__':
+    with open('result.json', mode='r') as f:
+        items = json.load(f)
+
+    def get_spec(item, prop):
+        specs = item["props"]["specs"]
+        matching_specs = [x for x in specs if x["name"].lower() == prop.lower()]
+        if len(matching_specs) == 0:
+            return None
+        
+        return matching_specs[0]["values"][0]
+
+    def get_packed_weight(item):
+        packaged_weight = get_spec(item, "packaged weight")
+        tokens = packaged_weight.split(' ')
+        pounds = float(tokens[0])
+        if len(tokens) >= 4:
+            pounds = pounds + (float(tokens[2])/16.0)
+        return pounds
+
+    def get_weight_safe(item):
+        try:
+            return get_packed_weight(item)
+        except:
+            return 9999
+
+    def get_sleeping_capacity(item):
+        return get_spec(item, "sleeping capacity")
+
+    items_to_sort = [{"link": x["link"], "weight": get_weight_safe(x), "sleeping capacity": get_sleeping_capacity(x)} for x in items]
+
+    items_to_sort.sort(key=lambda x: x["weight"])
+
+    for item in items_to_sort:
+        if (item["sleeping capacity"] == "2-person"):
+            print(item)
